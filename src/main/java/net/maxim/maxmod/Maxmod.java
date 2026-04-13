6aadbd7330061de0434765c714976ccdcc96b6a9
User <MaxMeleshkin2012@gmail.com>
2026-04-13 08:27:26 +0300

    всё сделал

--- src/main/java/net/maxim/maxmod/Maxmod.java
+++ src/main/java/net/maxim/maxmod/Maxmod.java
@@ -5,10 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -16,6 +13,12 @@ import net.minecraft.util.Identifier;
 
 public class Maxmod implements ModInitializer {
     public static final String MOD_ID = "maxmod";
+    public static final Item RUBY_SWORD = new SwordItem(RubyToolMaterial.INSTANCE,3,-2.4F,new Item.Settings());
+    public static final Item RUBY_PICKAXE = new PickaxeItem(RubyToolMaterial.INSTANCE,1,-2.8F,new Item.Settings());
+    public static final Item RUBY_SHOVEL = new ShovelItem(RubyToolMaterial.INSTANCE,1.5F,-3F,new Item.Settings());
+    public static final Item RUBY_AXE = new AxeItem(RubyToolMaterial.INSTANCE, 5, -3.0F, new Item.Settings());
+    public static final Item RUBY_HOE = new HoeItem(RubyToolMaterial.INSTANCE,-1,-2.0F,new Item.Settings());
+
 
     public static final Item RUBY = new Item(new Item.Settings());
     public static final Block RUBY_BLOCK = new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_BLOCK));
@@ -24,6 +27,12 @@ public class Maxmod implements ModInitializer {
             .entries((context, entries) -> {
                 entries.add(RUBY);
                 entries.add(RUBY_BLOCK);
+                entries.add(RUBY_SWORD);
+                entries.add(RUBY_PICKAXE);
+                entries.add(RUBY_SHOVEL);
+                entries.add(RUBY_AXE);
+                entries.add(RUBY_HOE);
+
             }).build();
 
     @Override
@@ -32,5 +41,10 @@ public class Maxmod implements ModInitializer {
         Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "ruby_group"), RUBY_GROUP);
         Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "ruby_block"), RUBY_BLOCK);
         Registry.register(Registries.ITEM, new Identifier(MOD_ID, "ruby_block"), new BlockItem(RUBY_BLOCK, new Item.Settings()));
+        Registry.register(Registries.ITEM,new Identifier(MOD_ID,"ruby_sword"),RUBY_SWORD);
+        Registry.register(Registries.ITEM,new Identifier(MOD_ID, "ruby_pickaxe"),RUBY_PICKAXE);
+        Registry.register(Registries.ITEM,new Identifier(MOD_ID, "ruby_axe"),RUBY_AXE);
+        Registry.register(Registries.ITEM,new Identifier(MOD_ID, "ruby_shovel"), RUBY_SHOVEL);
+        Registry.register(Registries.ITEM,new Identifier(MOD_ID, "ruby_hoe"),RUBY_HOE);
     }
 }
---
+++ src/main/java/net/maxim/maxmod/RubyToolMaterial.java
@@ -0,0 +1,40 @@
+package net.maxim.maxmod;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+import java.util.Map;
+
+public enum RubyToolMaterial implements ToolMaterial {
+    INSTANCE;
+
+    @Override
+    public int getDurability() {
+        return 1500;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 10.0F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 4.0F;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 3;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 15;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Maxmod.RUBY);
+    }
+}
